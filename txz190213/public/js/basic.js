58bc53e9c0ae8aad82a4589a313b539603727800
cykim <present1306@naver.com>
2020-11-19 11:10:26 +0900

    try emul map probolem resolve

--- txz190213/public/js/basic.js
+++ txz190213/public/js/basic.js
@@ -161,92 +161,101 @@ function showCurPos() {
     };
     map = new daum.maps.Map(mapContainer, mapOption);
 
-    function error(err) {
+    function showCurrentMap(lat, lng){
+        mapContainer = document.getElementById('map') // 지도를 표시할 div 
+        console.log("lat : " + lat + ", lng : " + lng);
+
+        
+        mapOption = {
+            center: new daum.maps.LatLng(lat, lng), // 지도의 중심좌표
+            level: 6// 지도의 확대 레벨
+        };
+        map = new daum.maps.Map(mapContainer, mapOption);
+        var markerPosition = new daum.maps.LatLng(lat, lng);
+        var marker = new daum.maps.Marker({
+            position: markerPosition
+        });
+        debugger;
+        marker.setMap(map);
+        var geocoder = new daum.maps.services.Geocoder();
+        geocoder.coord2Address(lng, lat, function (result, status) {
+            if (status === daum.maps.services.Status.OK) {
+                var curPosAddr = result[0].address.address_name;
+                $("#departInput").val(curPosAddr);
+                $("#departLatInMain").val(lat)
+                $("#departLngInMain").val(lng)
+                departDataOn.placename = curPosAddr;
+                departDataOn.jibunAddr = curPosAddr
+                departDataOn.lat = lat;
+                departDataOn.lng = lng;
+                isDepartSelected = true;
+            } else {
+                console.log("geocoder.coord2Address status err ");
+                console.log("status : ", status)
+                console.log("daum.maps.services.Status.OK : ", daum.maps.services.Status.OK)
+            }
+
+        });
+        infowindow = new daum.maps.InfoWindow({ zindex: 1 }); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
+        // clickMap(marker, geocoder);
+        daum.maps.event.addListener(map, 'click', function (mouseEvent) {
+            console.log('clicked lat : ' + mouseEvent.latLng.getLat());
+            console.log('clicked lng : ' + mouseEvent.latLng.getLng());
+            var coords = mouseEvent.latLng;
+            var clickPointLat = coords.getLat();
+            var clickPointLng = coords.getLng();
+            geocoder.coord2Address(clickPointLng, clickPointLat, function (result, status) {
+                if (status === daum.maps.services.Status.OK) {
+                    var detailAddr = !!result[0].road_address ?
+                        '<div class="addrItem">도로명주소 : '
+                        + result[0].road_address.address_name
+                        + '</div>' : '';
+                    detailAddr += '<div class="addrItem">지번 주소 : '
+                        + result[0].address.address_name
+                        + '</div>';
+                    console.log(result[0]);
+                    var content =
+                        '<div class="infoWindowBox">'
+                        + '<span class="title">법정동 주소정보</span>'
+                        + detailAddr
+                        + '</div>';
+                    marker.setPosition(mouseEvent.latLng);
+                    marker.setMap(map);
+
+                    // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
+                    infowindow.setContent(content);
+                    infowindow.open(map, marker);
+
+
+                    $("#latitude").val(clickPointLat);
+                    $("#longitude").val(clickPointLng);
+                    $("#addr").val(result[0].address.address_name);
+                    if (result[0].road_address) {
+                        $("#road_addr").val(result[0].road_address.address_name);
+                    } else {
+                        $("#road_addr").val("");
+                    }
+                }
+            });
+        });
+    }
+
+    function currentPositionErr(err) {
         console.warn('ERROR(' + err.code + '): ' + err.message);
+        showCurrentMap(lat, lng);
     };
 
     if (navigator.geolocation) {
+        
         navigator.geolocation.getCurrentPosition(function (pos) {
             console.log('nav geo 접근')
+            // debugger;
             console.log('pos : ' + JSON.stringify(pos));  //질문 빈객체로 나옴 아래는 출력됨
             lat = pos.coords.latitude;
             lng = pos.coords.longitude;
-            mapContainer = document.getElementById('map') // 지도를 표시할 div 
-            console.log("lat : " + lat + ", lng : " + lng);
-
-            
-            mapOption = {
-                center: new daum.maps.LatLng(lat, lng), // 지도의 중심좌표
-                level: 6// 지도의 확대 레벨
-            };
-            map = new daum.maps.Map(mapContainer, mapOption);
-            var markerPosition = new daum.maps.LatLng(lat, lng);
-            var marker = new daum.maps.Marker({
-                position: markerPosition
-            });
-            marker.setMap(map);
-            var geocoder = new daum.maps.services.Geocoder();
-            geocoder.coord2Address(lng, lat, function (result, status) {
-                if (status === daum.maps.services.Status.OK) {
-                    var curPosAddr = result[0].address.address_name;
-                    $("#departInput").val(curPosAddr);
-                    $("#departLatInMain").val(lat)
-                    $("#departLngInMain").val(lng)
-                    departDataOn.placename = curPosAddr;
-                    departDataOn.jibunAddr = curPosAddr
-                    departDataOn.lat = lat;
-                    departDataOn.lng = lng;
-                    isDepartSelected = true;
-                } else {
-                    console.log("geocoder.coord2Address status err ");
-                    console.log("status : ", status)
-                    console.log("daum.maps.services.Status.OK : ", daum.maps.services.Status.OK)
-                }
-
-            });
-            infowindow = new daum.maps.InfoWindow({ zindex: 1 }); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
-            // clickMap(marker, geocoder);
-            daum.maps.event.addListener(map, 'click', function (mouseEvent) {
-                console.log('mouseEvent.latLng.getLat() : ' + mouseEvent.latLng.getLat());
-                var coords = mouseEvent.latLng;
-                var clickPointLat = coords.getLat();
-                var clickPointLng = coords.getLng();
-                geocoder.coord2Address(clickPointLng, clickPointLat, function (result, status) {
-                    if (status === daum.maps.services.Status.OK) {
-                        var detailAddr = !!result[0].road_address ?
-                            '<div class="addrItem">도로명주소 : '
-                            + result[0].road_address.address_name
-                            + '</div>' : '';
-                        detailAddr += '<div class="addrItem">지번 주소 : '
-                            + result[0].address.address_name
-                            + '</div>';
-                        console.log(result[0]);
-                        var content =
-                            '<div class="infoWindowBox">'
-                            + '<span class="title">법정동 주소정보</span>'
-                            + detailAddr
-                            + '</div>';
-                        marker.setPosition(mouseEvent.latLng);
-                        marker.setMap(map);
-
-                        // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
-                        infowindow.setContent(content);
-                        infowindow.open(map, marker);
-
-
-                        $("#latitude").val(clickPointLat);
-                        $("#longitude").val(clickPointLng);
-                        $("#addr").val(result[0].address.address_name);
-                        if (result[0].road_address) {
-                            $("#road_addr").val(result[0].road_address.address_name);
-                        } else {
-                            $("#road_addr").val("");
-                        }
-                    }
-                });
-            });
+            showCurrentMap(lat, lng);
         }, 
-        error,
+        currentPositionErr,
         { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }
         );
 
